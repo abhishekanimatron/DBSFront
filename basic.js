17df3cb8b5a1e4f92b76db41fc157beac551f369
Abhishek Verma <44282781+abhishekanimatron@users.noreply.github.com>
2020-02-26 10:38:59 +0530

    Changes 10:38AM

---
+++ basic.js
@@ -0,0 +1,21 @@
+document.getElementById("textboxId").style.display = "none";
+
+function myFunction1() {
+  document.getElementById("schedule").innerHTML = "Schedule of the Train ?";
+  document.getElementById("textboxId").style.display = "block";
+}
+
+function myFunction2() {
+  document.getElementById("emergency").innerHTML = "What's the emergency?";
+  document.getElementById("textboxId").style.display = "block";
+}
+
+function myFunction3() {
+  document.getElementById("location").innerHTML = "Which train's location?";
+  document.getElementById("textboxId").style.display = "block";
+}
+
+function myFunction4() {
+  document.getElementById("others").innerHTML = "Mention Your Query:";
+  document.getElementById("textboxId").style.display = "block";
+}
