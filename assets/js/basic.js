51a80f8eea02e37072fd7f56e33f6f1a1e1cabdd
Abhishek Verma <44282781+abhishekanimatron@users.noreply.github.com>
2020-02-26 16:10:27 +0530

    update new 4:09PM

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -1,18 +1,7 @@
-document.getElementById("textboxId").style.display = "none";
-
-function showDivText() {
-  var my_disply = document.getElementById("textboxId").style.display;
-  if (my_disply == "block")
-    document.getElementById("textboxId").style.display = "none";
-  else document.getElementById("textboxId").style.display = "block";
+function showInput() {
+  document.getElementById("display").innerHTML = document.getElementById(
+    "user_input"
+  ).value;
 }
 
-$("input[type='text']").keypress(function(event) {
-  if (event.which === 13) {
-    //grab the newTodo text from input
-    var todoText = $(this).val();
-    $(this).val("");
-    $("ul").append("<li>" + todoText + "</li>");
-  }
-});
 
